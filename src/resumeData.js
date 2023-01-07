95aeb5f74d7c7cca36898faca4094ea53d3a099b
ElMourabit-Mohammed <elmourabitmohammed00@gmail.com>
2023-01-07 13:20:38 +0100

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,100 @@
+let resumeData = {
+    "name": "El mourabit Mohammed",
+    "role": "Software Engineer Student At ENSAO",
+    "linkedinId":"elmourabitmohammed00",
+    "roleDescription": "As a software engineer student, I am highly motivated and dedicated to learning and growing my skills in the field. I have a strong foundation in computer science and am proficient in a variety of programming languages, including Java, Python, and C++. I am also skilled in using development tools such as Git, Eclipse, and Visual Studio. I have completed a number of software engineering projects, both as part of my academic coursework and as personal projects. These projects have given me experience in the full software development life cycle, including requirements gathering, design, implementation, testing, and maintenance.I am a team player with strong communication skills and have experience working on projects in a collaborative environment. I am also proactive in seeking out opportunities to learn and improve my skills, and am always looking for new challenges to tackle.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/elmourabitmohammed00/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/ElMourabit-Mohammed",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am a software engineer student with a passion for problem-solving and creating innovative solutions through code. With a strong foundation in computer science and proficiency in multiple programming languages, I am eager to continue learning and growing as a developer. I am a team player and have experience collaborating on projects, and am always looking for opportunities to challenge myself and improve my skills.",
+    "address":"Morocco",
+    "website":"https://github.com/ElMourabit-Mohammed",
+    "education":[
+      {
+        "UniversityName":"National School Of Applied Sciences Oujda - ENSAO",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2021",
+        "Achievements":"Graduation in 2024"
+      },
+      {
+        "UniversityName":"National School Of Applied Sciences Oujda - ENSAO",
+        "specialization":"STPI",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2019",
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"OurMark",
+        "specialization":"Web Application",
+        "MonthOfLeaving":"Html5 Css3 JavaScript Php",
+      },
+      {
+        "CompanyName":"TechQuiz",
+        "specialization":"Android Application",
+        "MonthOfLeaving":"Android Studio - Java & XML",
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS3"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Java"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"TechQuiz",
+        "description":"Android Application",
+        "imgurl":"images/Androidproject.jpg"
+      },
+      {
+        "name":"E-School",
+        "description":"JavaFx Project",
+        "imgurl":"images/Javafxproject.jpg"
+      },
+      {
+        "name":"OurMark",
+        "description":"Web Application",  
+        "imgurl":"images/Webproject.jpg"
+      },
+      {
+        "name":"OurMark",
+        "description":"Web Application",  
+        "imgurl":"images/Webproject.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"I had the pleasure of working with El mourabit Mohammed on a software development project for our company. From the start, El mourabit impressed me with their strong work ethic and dedication to delivering high-quality results. Their technical skills and attention to detail were instrumental in the success of the project.",
+        "name":"Client Number 1"
+      },
+      {
+        "description":"I highly recommend El mourabit Mohammed for any software development project. he is a talented and reliable developer who consistently delivers excellent results.",
+        "name":"Client Number 2"
+      }
+    ]
+  }
+  
+  export default resumeData
